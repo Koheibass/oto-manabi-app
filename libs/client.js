bf85384358c62e56f99ad485a5ce54c29a1e1af3
koheis <kouheihand@yahoo.co.jp>
2024-03-15 12:33:30 +0900

    セッティング

---
+++ libs/client.js
@@ -0,0 +1,6 @@
+import { createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+    serviceDomain: 'oto-manabi',
+    apiKey: 'LajtL9M6uxqtVfzNx3gypEHlFHpKGtsL05mm',
+});
---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { client } from "/libs/client";
+
+export default function Home({ contents }) {
+  return (
+    <div>
+      <ul>
+        {contents.map((content) => (
+          <li key={content.id}>
+            <Link href={content.videoUrl}>
+              <img className="w-40" src={content.thumbnail.url} art={content.name} />
+              {content.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <p></p>
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+  const data = await client.get({
+    endpoint: "content", queries: { filters: 'type[contains]質問Liveアーカイヴ' }
+  })
+
+  return {
+    props: {
+      contents: data.contents,
+    },
+  };
+};
