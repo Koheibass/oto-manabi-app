5e1772cfed7a854c47f16f5a9b4a2bcbc39e3f6c
koheis <kouheihand@yahoo.co.jp>
2024-04-20 22:14:33 +0900

    ボトムナビゲーションのCSS修正

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -6,35 +6,29 @@ import { RiMentalHealthLine } from "react-icons/ri";
 import { MdLibraryMusic } from "react-icons/md";
 import { MdOutlineMenuBook } from "react-icons/md";
 
-
-
-
 export default function BottomNav() {
-
     return (
-        <IconContext.Provider value={{ color: 'black', size: '30px' }}>
-            <ul className='flex bg-[#ffe6fd] fixed bottom-0 left-0 right-0 z-10 justify-center text-center'>
-                <li className='pl-'>
-                    <Link href='/'><IoMdHome /></Link>
-                    <span className='text-xs'>ホーム</span>
-                </li>
-                <li className='pl-8'>
-                    <Link href='/Maincontents/questioncontent'><TbMusicQuestion /></Link>
-                    <span className='text-[10px]'>質問Live</span>
-                </li>
-                <li className='pl-8'>
-                    <Link href='/Maincontents/mentalcontent'><RiMentalHealthLine /></Link>
-                    <span className='text-[10px]'>メンタル</span>
-                </li>
-                <li className='pl-8'>
-                    <Link href='/Maincontents/mgrammercontent'><MdLibraryMusic /></Link>
-                    <span className='text-[10px]'>楽典</span>
-                </li>
-                <li className='pl-8'>
-                    <Link href='/Maincontents/columncontent'><MdOutlineMenuBook /></Link>
-                    <span className='text-[10px]'>Rinaコラム</span>
-                </li>
-            </ul>
-        </IconContext.Provider>
+        <ul className='flex bg-[#ffe6fd] fixed bottom-0 left-0 right-0 z-10 justify-center text-center'>
+            <li className='pl-'>
+                <Link href='/'><IoMdHome className='text-[30px]' /></Link>
+                <span className='text-xs'>ホーム</span>
+            </li>
+            <li className='pl-8'>
+                <Link href='/Maincontents/questioncontent'><TbMusicQuestion className='text-[30px]' /></Link>
+                <span className='text-[10px]'>質問Live</span>
+            </li>
+            <li className='pl-8'>
+                <Link href='/Maincontents/mentalcontent'><RiMentalHealthLine className='text-[30px]' /></Link>
+                <span className='text-[10px]'>メンタル</span>
+            </li>
+            <li className='pl-8'>
+                <Link href='/Maincontents/mgrammercontent'><MdLibraryMusic className='text-[30px]' /></Link>
+                <span className='text-[10px]'>楽典</span>
+            </li>
+            <li className='pl-8'>
+                <Link href='/Maincontents/columncontent'><MdOutlineMenuBook className='text-[30px]' /></Link>
+                <span className='text-[10px]'>Rinaコラム</span>
+            </li>
+        </ul>
     );
 }
